47bb27a6ba18eb650cb1c798907831613d8ba063
aniljainbaba <anil.jain.baba@gmail.com>
2021-07-21 12:34:21 +0530

    added the move class

---
+++ src/com/Move.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Move  implements MoveInterface{
+
+	@Override
+	public int rollDice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isLandingOnSnake() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLandingOnLadder() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}//end Move
---
+++ src/com/MoveInterface.java
@@ -0,0 +1,11 @@
+package com;
+
+public interface MoveInterface {
+  
+	public int rollDice();
+	
+	public boolean isLandingOnSnake(); 
+	
+	public boolean isLandingOnLadder();
+
+}//end interface
