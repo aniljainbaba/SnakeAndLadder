719971da5a497239930295cdd3701a1559712595
aniljainbaba <anil.jain.baba@gmail.com>
2021-07-21 12:14:07 +0530

    added the player component

---
+++ src/com/Player.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Player implements PlayerInterface{
+
+	@Override
+	public String getPlayerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position getPlayerCurrentPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}//end class
---
+++ src/com/PlayerInterface.java
@@ -0,0 +1,8 @@
+package com;
+
+public interface PlayerInterface {
+	
+	String getPlayerName();
+	Position getPlayerCurrentPosition();
+
+}
